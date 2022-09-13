a1c3b6c8268b5497f53e5c266192f2350ce88078
FedEgues <eguesf@gmail.com>
2022-09-13 12:06:55 -0300

    Revert "ultimo cambio"
    This reverts commit e06df727849ff8fc8910b833786c30078296517b.

--- src/main/java/com/sitrack/testEgues/TestEguesApplication.java
+++ src/main/java/com/sitrack/testEgues/TestEguesApplication.java
@@ -1,29 +1,14 @@
 package com.sitrack.testEgues;
 
-import com.sitrack.testEgues.servicios.ServicioOpciones;
-import java.util.Scanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
+@SpringBootApplication
 public class TestEguesApplication {
 
 	public static void main(String[] args) {
-		  int i =0;
-       ServicioOpciones ser = new ServicioOpciones();
-       do{
-           ser.opciones();
-           System.out.println("¿Desea continuar buscando?");
-           System.out.println("Ingrese 0 para Si");
-           System.out.println("Ingrese 1 para No");
-           Scanner leer = new Scanner(System.in).useDelimiter("\n");
-           try{
-           i= leer.nextInt();
-           }catch(Exception e){
-               System.out.println("Ingresó una opción inválida.");
-               ser.opciones();
-           }
-       }while(i==0);
-       
-    }
+		SpringApplication.run(TestEguesApplication.class, args);
+               
+	}
 
 }
